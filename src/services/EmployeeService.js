2e669b86d14695bffb474bd315bcb1a4ffa2f06a
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-12 22:41:43 +0530

    MODIFIED: Use Axios service class to call JSON server API

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,9 +1,9 @@
 import config from "../config/config";
-const axios = require('axios').default;
+import AxiosService from "./AxiosService.js";
 
 export default class EmployeeService {
   baseUrl = config.baseUrl;
   addEmployee(data) {
-    return axios.post(`${this.baseUrl}employee`, data);
+    return AxiosService.postService(`${this.baseUrl}employee`, data);
   }
-} 
+}
