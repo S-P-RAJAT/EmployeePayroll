1903508789caa6b174f2daa9fe9e65280099b7ca
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-14 22:08:33 +0530

    ADDED: Route is set for home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import PayrollForm from "./components/payroll-form/PayrollForm";
+import EmployeeHome from "./components/payroll-home/PayrollHome";
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,14 +11,25 @@ import {
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Switch>
-          <Route exact path="">
-            <PayrollForm />
-          </Route>
-        </Switch>
-      </Router>
+    <div className="App">
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/home">
+              <EmployeeHome />
+            </Route>
+            <Route exact path="/payroll-form">
+              <PayrollForm />
+            </Route>
+            <Route exact path="/payroll-form/:id">
+              <PayrollForm />
+            </Route>
+            <Route exact path="">
+              <Redirect exact from="/" to="/home" />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
